a606537ed44ca2ac8cc995ddd854c175bccca617
Antony Ng'ang'a <antony_mn@Antonys-MBP.mshome.net>
2017-10-12 02:39:51 +0300

    fetched && displayed weather data of hard coded location

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -7,9 +7,26 @@ import { WeatherProvider } from '../../providers/weather/weather';
   templateUrl: 'home.html'
 })
 export class HomePage {
+  weather:any;
+  location:{
+    city:string,
+    state:string
+  }
 
   constructor(public navCtrl: NavController, private weatherProvider:WeatherProvider) {
 
   }
 
+
+  ionViewWillEnter(){
+    this.location = {
+      city: 'Miami',
+      state: 'FL'
+    }
+
+    this.weatherProvider.getWeather(this.location.city, this.location.state).subscribe(weather => {
+      console.log(weather)
+      this.weather = weather.current_observation;
+    });
+  }
 }
